31a07c9a6707877ee50683345a2d9336bad37fa2
johanneslares <Johq.lares@gmail.com>
2020-11-01 14:09:13 +0200

    Tehtävän 8 rivikattavuus 100%

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -34,7 +34,7 @@ public class VarastoTest {
     public void lisaysLisaaSaldoa() {
         varasto.lisaaVarastoon(8);
 
-        // saldon pit√§isi olla sama kun lis√§tty m√§√§r√§
+        // saldon pit‰isi olla sama kun lis‰tty m‰‰r‰
         assertEquals(8, varasto.getSaldo(), vertailuTarkkuus);
     }
 
@@ -42,7 +42,7 @@ public class VarastoTest {
     public void lisaysLisaaPienentaaVapaataTilaa() {
         varasto.lisaaVarastoon(8);
 
-        // vapaata tilaa pit√§isi viel√§ olla tilavuus-lis√§tt√§v√§ m√§√§r√§ eli 2
+        // vapaata tilaa pit‰isi viel‰ olla tilavuus-lis‰tt‰v‰ m‰‰r‰ eli 2
         assertEquals(2, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
@@ -56,13 +56,73 @@ public class VarastoTest {
     }
 
     @Test
-    public void ottaminenLis√§√§Tilaa() {
+    public void ottaminenLisaaTilaa() {
         varasto.lisaaVarastoon(8);
 
         varasto.otaVarastosta(2);
 
-        // varastossa pit√§isi olla tilaa 10 - 8 + 2 eli 4
+        // varastossa pit‰isi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
+    
+    @Test
+    public void konstruktoriToimiiAlleNolla() {
+    	Varasto uusi = new Varasto(-2);
+    	assertEquals(0, uusi.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void konstruktoriToimiiAlleNollaAlkuSaldo() {
+    	Varasto uusi = new Varasto(0, 0);
+    	assertEquals(0, uusi.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void konstruktoriToimiiAlkuSaldoAlleNolla() {
+    	Varasto uusi = new Varasto(10, -2);
+    	assertEquals(10, uusi.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void konstruktoriToimiiAlkuSaldoYliTilavuus() {
+    	Varasto uusi = new Varasto(10, 20);
+    	assertEquals(0, uusi.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+    
 
+    @Test
+    public void konstruktoriToimiiYliNollaAlkuSaldo() {
+    	Varasto uusiVarasto = new Varasto(10, 5);
+    	assertEquals(5, uusiVarasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void lisaaVarastoonAlleNolla() {
+    	varasto.lisaaVarastoon(-1);
+    	assertEquals(10, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void varastoOverFlow() {
+    	varasto.lisaaVarastoon(100);
+    	assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void ottaminenAlleNolla() {
+    	varasto.lisaaVarastoon(5);
+    	assertEquals(0.0, varasto.otaVarastosta(-2), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void kaikkiMitaSaadaan() {
+    	varasto.lisaaVarastoon(5);
+    	assertEquals(5, varasto.otaVarastosta(10), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void tekstia() {
+    	varasto.lisaaVarastoon(2);
+    	assertEquals("saldo = 2.0, viel‰ tilaa 8.0", varasto.toString());
+    }
 }
